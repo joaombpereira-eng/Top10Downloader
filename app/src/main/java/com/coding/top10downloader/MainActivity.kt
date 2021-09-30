a88dad503ebfb4de16a4ad351abb5c5b17fcf432
João Pereira <joaopereira.eng.tec@gmail.com>
2021-09-30 14:50:31 +0100

    no message

---
+++ app/src/main/java/com/coding/top10downloader/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.coding.top10downloader
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.net.URL
+
+class FeedEntry {
+    var name: String = ""
+    var artist: String = ""
+    var releaseDate: String = ""
+    var summary: String = ""
+    var imageURL: String = ""
+}
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "OnCreate: Called")
+        val downloadData = DownloadData()
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml")
+        Log.d(TAG, "OnCreate: Done")
+    }
+
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "onPostExecute: Parameter is $result")
+            }
+
+            override fun doInBackground(vararg url: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${url[0]}")
+                val rssFeed = downloadXML(url[0])
+                if (rssFeed.isEmpty()) {
+                    Log.e(TAG, "doInBackground: Error Downloading")
+                }
+                return rssFeed
+            }
+
+            private fun downloadXML(urlPath: String?): String {
+                return URL(urlPath).readText()
+            }
+        }
+    }
+}
